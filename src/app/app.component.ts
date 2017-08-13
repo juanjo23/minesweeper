fd1e3b6eb45e1ff04686755ae377a2182693dfbf
Juan Miramontes <juan.jose.sandoval@accenture.com>
2017-08-13 03:09:53 -0500

    Adding mineswasper logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Platform } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
-
+  private rootPage: any = HomePage;
+  public pages: {title: string}[];
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      this.pages = [
+        {title: 'Home'},
+        {title: 'About'},
+        {title: 'Contact'}
+      ];
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+  public openPage(p: {title: string}): void {
+    alert(p.title);
+  }
 }
-
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MineLogic } from './minelogic';
+import { Box } from './minelogic';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public mines: Box[][];
 
   constructor(public navCtrl: NavController) {
-
+    const a: any = new MineLogic();
+    this.mines = a.mines;
+    a.printMines();
   }
 
 }
